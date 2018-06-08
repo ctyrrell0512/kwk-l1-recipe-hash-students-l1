b56857ef8820580e8fd1d0e3a497a7d29cd386af
ctyrrell0512 <colleen.a.tyrrell@gmail.com>
2018-06-08 16:03:26 +0000

    automatically backed up by learn

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -25,7 +25,7 @@ def recipe_ingredients(perfect_10_recipe)
   return perfect_10_recipe
 end
 
-recipe_ingredients(perfect_10_recipe.values[7])
+ puts recipe_ingredients(perfect_10_recipe.values[7])
 
 
 
