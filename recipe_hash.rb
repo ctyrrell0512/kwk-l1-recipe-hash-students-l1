8d1df3630a90e4543071c709b41eebea6879284e
ctyrrell0512 <colleen.a.tyrrell@gmail.com>
2018-06-08 15:56:15 +0000

    automatically backed up by learn

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -3,17 +3,17 @@
 #1. Create a hash called `perfect_10_recipe` that has the following data:
 perfect_10_recipe= {}
 
-# perfect_10_recipe[:almond_flour] = "2 cups"
-# perfect_10_recipe[:gluten_freewhole_oats] = "1 cup"
-# perfect_10_recipe[:kosher_salt] = "1/2 tea spoon"
+perfect_10_recipe[:almond_flour] = "2 cups"
+perfect_10_recipe[:gluten_freewhole_oats] = "1 cup"
+perfect_10_recipe[:kosher_salt] = "1/2 tea spoon"
 perfect_10_recipe[:baking_powder]  = "1/2 tea spoon"
 perfect_10_recipe[:baking_soda] = "1/4 tea spoon"
 perfect_10_recipe[:Xanthan_Gum] = "1/2 tea spoon"
 perfect_10_recipe[:slivered_almonds_lightly_toasted] = "1/4 cup"
-# perfect_10_recipe[:mini_dark_chocolate_chips] = "3/4 cup"
-# perfect_10_recipe[:olive_oil] = "1/2 cup"
-# perfect_10_recipe[:agave] = "1/4 cup"
-# # All of the ingredients should be symbols and the amounts should be strings. Remember that symbols start with a colon and be sure to sub underscores for spaces.
+perfect_10_recipe[:mini_dark_chocolate_chips] = "3/4 cup"
+perfect_10_recipe[:olive_oil] = "1/2 cup"
+perfect_10_recipe[:agave] = "1/4 cup"
+# All of the ingredients should be symbols and the amounts should be strings. Remember that symbols start with a colon and be sure to sub underscores for spaces.
 # puts perfect_10_recipe
 
 # perfect_10_recipe = {:almond_flour=>"2 cups", :gluten_free_whole_oats=>"1 cup", :kosher_salt=>"1/2 tea spoon", :baking_po...almonds=>"1/4 cup", :mini_dark_chocolate_chips=>"3/4 cup", :olive_oil=>"1/2 cup", :agave=>"1/4 cup"}
